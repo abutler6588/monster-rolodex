42ecc110614b21a25c22e0f05ae40b9d943e40b7
System Administrator <root@Andres-Air.attlocal.net>
2020-10-12 15:46:40 -0500

    Updates

---
+++ precache-manifest.835a9b4a375197c8474b1862bbfca243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c223b99f45531f065fdbec3563e6a1e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "7a30003c428bb667027d",
+    "url": "/monster-rolodex/static/css/main.45cbc25c.chunk.css"
+  },
+  {
+    "revision": "5a5b5d4d61784f131205",
+    "url": "/monster-rolodex/static/js/2.89420f1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.89420f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a30003c428bb667027d",
+    "url": "/monster-rolodex/static/js/main.0269f41a.chunk.js"
+  },
+  {
+    "revision": "cbfb05f6f66b25104b24",
+    "url": "/monster-rolodex/static/js/runtime-main.a938ba23.js"
+  }
+]);
